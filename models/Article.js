136f65bbbd5513f555a2587a791ac61725c17a51
thePhilX <Philipp@grossX.de>
2018-01-27 01:04:52 +0100

    added timestamp property for articles

--- models/Article.js
+++ models/Article.js
@@ -4,8 +4,9 @@ var mongoose = require('mongoose'),
 var ArticleSchema = new Schema({
     title: { type: String, required: true },
     description: {type: String, required: true},
+    price: { type: Number, required: true },
     requirements: { type: String, required: true },
     deliveryTime: {type: Number, required: true},
-    price: { type: Number, required: true },
+    timestamp: {type: Date, required: true, default: Date.now() }
 });
 module.exports = mongoose.model('Article', ArticleSchema);
